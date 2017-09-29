2a75f06cb09fc0cf814b27ec87634acbc022f84e
yochie <yoann.ochietti@gmail.com>
2017-09-28 22:53:21 -0400

    Fixed bugs, added loads of comments, made baseline compute less often

--- Read_Resistance/Read_Resistance.ino
+++ Read_Resistance/Read_Resistance.ino
@@ -1,24 +1,38 @@
 const int potPin1 = 1;    // select the input pin for the potentiometer
 //int potPin2 = 2;    // select the input pin for the potentiometer
-//int potPin3 = 2;    // select the input pin for the potentiometer
-//int potPin4 = 2;    // select the input pin for the potentiometer
+//int potPin3 = 3;    // select the input pin for the potentiometer
+//int potPin4 = 4;    // select the input pin for the potentiometer
 
 const short BUFFER_SIZE = 256; //amount of vals that we average baseline over
-const short JUMP_BUFFER_SIZE = 64; //max amount of jump vals used to average press velocity
 
-const short JUMP_THRESHOLD = 50; //Difference in value that qualifies as a press
-const short JUMP_VARIABILITY = 30; //How much a jump can differ from the last to qualify as consecutive
-const short MAX_CONSECUTIVE_JUMPS = JUMP_BUFFER_SIZE * 4; //After this amount of consecutive (and similar) jumps is reached
-//, the baseline is reset to that jump sequences avg velocity
+//Max amount of jump vals used to average press velocity. If a button is kept pressed,
+//a jump message will be printed every time the buffer is full.
+//Avoid making too large as then jump messages would'nt be printed often enough
+const short JUMP_BUFFER_SIZE = 64;
 
-const int BAUD_RATE = 19200;
+//Difference in value that qualifies as a press
+const short JUMP_THRESHOLD = 50;
+
+//How much a jump can differ from the last to qualify as consecutive
+//make sure its no larger than 1024...
+const short JUMP_VARIABILITY = 30;
+
+//After this amount of consecutive (and similar) jumps is reached,
+//the baseline is reset to that jump sequences avg velocity
+const short MAX_CONSECUTIVE_JUMPS = JUMP_BUFFER_SIZE * 4;
+
+
+//How frequently do we add an element to the baseline buffer. Used so that we dont compute baseline so often.
+const short CYCLES_PER_BASELINE = 10;
+
+const int BAUD_RATE = 9600;
 
 int val = 0;
 int cnt = 0;
 short baselineBuffer[BUFFER_SIZE];
 short jumpBuffer[JUMP_BUFFER_SIZE];
 int baseline = 0;
-int lastJumpVal = 2048;
+int lastVal = 2048;
 int waitCycles = 0;
 int jumpCount = 0;
 int consecutiveJumpCount = 0;
@@ -27,65 +41,81 @@ void setup() {
   Serial.begin(BAUD_RATE);      // open the serial port at x bps:
 
   //establish first baseline using 100 (arbitrary) values
+  //this baseline will be updated throughout the loop
   for (int i = 0; i < 100; i++) {
     baseline += analogRead(potPin1);
   }
-
   baseline = baseline / 100;
 }
 
-// read the value from the sensor
 void loop() {
-  //New Val
-  val = (short) analogRead(potPin1);
-
-  //If buffer is full, compute its baseline and reset counter
-  if (cnt == BUFFER_SIZE) {
+  //If baseline buffer is full, compute its average and reset its counter
+  if (cnt == BUFFER_SIZE*CYCLES_PER_BASELINE) {
     cnt = 0;
     baseline = computeAverage(baselineBuffer, BUFFER_SIZE);
     //    Serial.print("Computed new baseline : ");
     //    Serial.println(baseline);
   }
+
+  //New Val
+  val = (short) analogRead(potPin1);
   short jumpVal = val - baseline;
-  //If moved up by JUMP_THRESHOLD or more from baseline, save jump to buffer.
-  if (jumpVal > JUMP_THRESHOLD) {
+
+  //If jump is large enough, save val to buffer and print average jump if its full.
+  //Also makes sure that we don't get stuck in jump by restablishing baseline after some stagnation
+  if (jumpVal >= JUMP_THRESHOLD) {
+    //If we get many consecutive jumps without much variability, reset baseline.
+    //2048 is default value for lastVal, so it will never match on this condition 
+    //since val is between [0, 1024] and variability should be no larger than 1024
+    if (abs(lastVal - val) < JUMP_VARIABILITY) {
+      consecutiveJumpCount++;
+      if (consecutiveJumpCount >= MAX_CONSECUTIVE_JUMPS) {
+        baseline = computeAverage(jumpBuffer, jumpCount);
+        consecutiveJumpCount = 0;
+        jumpCount = 0;
+        lastVal = 2048;
+        cnt = 0;
+      }
+    }
     //If there is place in buffer, add jump there.
-    if (jumpCount < JUMP_BUFFER_SIZE) {
-      jumpBuffer[jumpCount] = jumpVal;
+    else if (jumpCount < JUMP_BUFFER_SIZE) {
+      //put absolute val (not jumpVal) in buffer
+      jumpBuffer[jumpCount] = val;
       jumpCount++;
+
+      //store current val for stagnation check
+      lastVal = val;
     }
-    //print buffer to serial and reset counter
+    //If buffer is full, print buffer average to serial and reset counter
+    //This means we send one value every JUMP_BUFFER_SIZE iterations of the
+    //main loop if the button is held down. Making JUMP_BUFFER_SIZE too large would
+    //thus cause timing issues here.
     else {
-      short avgVelocity = computeAverage(jumpBuffer, jumpCount);
+      short avgVal = computeAverage(jumpBuffer, jumpCount);
       Serial.print("J: ");
-      Serial.println(avgVelocity);
+      Serial.println(constrain(avgVal - baseline, 0, 512));
       jumpCount = 0;
+      lastVal = val;
     }
-    lastJumpVal = val;
     //Serial.print("jumps : ");
     //Serial.println(jumpCount);
-    //If we get many consecutive jumps without much variability, reset baseline.
-//        if (lastJumpVal - JUMP_VARIABILITY < val  && val < lastJumpVal + JUMP_VARIABILITY) {
-//          consecutiveJumpCount++;
-//          if (consecutiveJumpCount >= MAX_CONSECUTIVE_JUMPS) {
-//            baseline = computeAverage(jumpBuffer, JUMP_BUFFER_SIZE);
-//            consecutiveJumpCount = 0;
-//          }
-//        }
 
   }
   //Add value to buffer for baseline update and reset jump counting variables
   else {
-    //if last cycle was jump, we should send the jump data to the serial
-    if (lastJumpVal != 2048 && jumpCount > 0) {
-      short avgVelocity = computeAverage(jumpBuffer, jumpCount);
+    //if last cycle was jump, we should send the remaining jump data to the serial
+    if (lastVal != 2048 && jumpCount > 0) {
+      short avgVal = computeAverage(jumpBuffer, jumpCount);
       Serial.print("J: ");
-      Serial.println(avgVelocity);
+      Serial.println(avgVal);
     }
-    baselineBuffer[cnt] = val;
+    //every 10 loops, add value to baseline buffer for updating baseline
+    if (cnt % CYCLES_PER_BASELINE == 0) {
+      baselineBuffer[(int) cnt / CYCLES_PER_BASELINE] = val;
+    }
     cnt++;
     //Using 2048 as default value that will never match the current val when testing for consecutive jumps
-    lastJumpVal = 2048;
+    lastVal = 2048;
     jumpCount = 0;
     Serial.println("J: 0");
   }
