15cc4e9b2e50db98641eaf1bdf48609f7f17792e
yoann.ochietti@gmail.com <yoann.ochietti@gmail.com>
2017-12-06 14:50:00 -0500

    Set unsigned to relevant global variables and constants. Also reviewed integer sizes (long, short, int): set all cycle counters to long and numbers that stay in [0, 1024] to shorts.

--- Read_Resistance/Read_Resistance.ino
+++ Read_Resistance/Read_Resistance.ino
@@ -1,73 +1,73 @@
 //CONFIGURATION
 
 //Sensor pins
-const int NUM_SENSORS = 1;
-const int PINS[NUM_SENSORS] = {0};
+unsigned const short NUM_SENSORS = 1;
+unsigned const short PINS[NUM_SENSORS] = {0};
 
 //Serial communication Hz
-const int BAUD_RATE = 115200;
+unsigned const long BAUD_RATE = 115200;
 
 //Controller clock rate in MHz
 //to be used in cycle dependent settings
-const int CLOCK_RATE = 180;
+unsigned const short CLOCK_RATE = 180;
 
 //amount of vals that we average baseline over
-const short BUFFER_SIZE = 512;
+unsigned const short BUFFER_SIZE = 512;
 
 //How frequently do we add an element to the baseline buffer. 
 //Used to average baseline over longer duration without having too many values to process
-const short CYCLES_PER_BASELINE = 1;
+unsigned const short CYCLES_PER_BASELINE = 1;
 
 //Amount of jump vals used to buffer press velocity. If a button is kept pressed,
 //a jump message will be printed every time the buffer is full.
 //Avoid making too large as then short signals will be ignored
-const short JUMP_BUFFER_SIZE = 48;
+unsigned const short JUMP_BUFFER_SIZE = 48;
 
 //Difference in value from baseline that qualifies as a press
 //MAX_THRESHOLD is used when signal moves around
 //MIN_THRESHOLD is used when signal is flat
-const short MAX_THRESHOLD = 50;
-const short MIN_THRESHOLD = 30;
+unsigned const short MAX_THRESHOLD = 50;
+unsigned const short MIN_THRESHOLD = 30;
 
 //After this amount of consecutive (and non-varying) jumps is reached,
 //the baseline is reset to that jump sequences avg velocity
 unsigned const long MAX_CONSECUTIVE_JUMPS = CLOCK_RATE * 500;
-unsigned const int CONSECUTIVE_JUMP_BUFFER_SIZE = 1024;
-unsigned const int CYCLES_PER_CJUMP = MAX_CONSECUTIVE_JUMPS / CONSECUTIVE_JUMP_BUFFER_SIZE;
+unsigned const short CONSECUTIVE_JUMP_BUFFER_SIZE = 1024;
+unsigned const long CYCLES_PER_CJUMP = MAX_CONSECUTIVE_JUMPS / CONSECUTIVE_JUMP_BUFFER_SIZE;
 
 //How much a jump can differ from the last to qualify as "consecutive"
 //make sure its in the range [0, 1024]
-const short JUMP_VARIABILITY = 128;
+unsigned const short JUMP_VARIABILITY = 128;
 
 //Minimum number of cycles that a jump must last for it to need blowback compensation
 //Setting as multiple of JUMP_BUFFER_SIZE so that we know how many values were printed
-const short MIN_JUMPS = 10*JUMP_BUFFER_SIZE;
+unsigned const long MIN_JUMPS = 10*JUMP_BUFFER_SIZE;
 
 //number of cycles after jump during which input is ignored
-const int JUMP_BLOWBACK = 32;
+unsigned const short JUMP_BLOWBACK = 32;
 
 //GLOBAL VARIABLES
 
 //main iterator: countes the number of loop() executions while not jumping
-int cnt[NUM_SENSORS];
+unsigned long cnt[NUM_SENSORS];
 
 //array used to compute baseline while not jumping
-short baselineBuffer[NUM_SENSORS][BUFFER_SIZE];
+unsigned short baselineBuffer[NUM_SENSORS][BUFFER_SIZE];
 
 //small buffer used to signal jumps
 //the average of this array is printed every time it becomes full
-short jumpBuffer[NUM_SENSORS][JUMP_BUFFER_SIZE];
-int jumpIndex[NUM_SENSORS];
+unsigned short jumpBuffer[NUM_SENSORS][JUMP_BUFFER_SIZE];
+unsigned short jumpIndex[NUM_SENSORS];
 
 //this is a larger scale version of the jumpBuffer index
 //it is used to reset baseline when MAX_CONSECUTIVE_JUMPS consecutive jumps occur
-short cjumpBuffer[NUM_SENSORS][CONSECUTIVE_JUMP_BUFFER_SIZE];
+unsigned short cjumpBuffer[NUM_SENSORS][CONSECUTIVE_JUMP_BUFFER_SIZE];
 
 //number of consecutive jumps in cycles (not all are stored)
 unsigned long consecutiveJumpCount[NUM_SENSORS];
 
 //number of stored jumps in the cjumpBuffer
-unsigned int consecutiveJumpIndex[NUM_SENSORS];
+unsigned short consecutiveJumpIndex[NUM_SENSORS];
 
 //flag indicating that a sensor had just jumped
 //used to initiate blowback waiting phase
@@ -75,19 +75,19 @@ bool jumped[NUM_SENSORS];
 
 //number of cycles left to recuperate from blowback
 //(erratic/low values after pressing sensor)
-int toWait[NUM_SENSORS];
+unsigned short toWait[NUM_SENSORS];
 
 //holds current baseline for each pin
-int baseline[NUM_SENSORS];
+unsigned short baseline[NUM_SENSORS];
 
 //last read value for each pin
-int lastVal[NUM_SENSORS];
+unsigned short lastVal[NUM_SENSORS];
 
 //current pin index
-short currentSensor;
+unsigned short currentSensor;
 
 //current threshold 
-short jump_threshold;
+unsigned short jump_threshold;
 
 void setup() {
   Serial.begin(BAUD_RATE);      // open the serial port at x bps:
@@ -128,8 +128,8 @@ void loop() {
   if (cnt[currentSensor] > (BUFFER_SIZE - 1) * CYCLES_PER_BASELINE) {
 
     //adjust threshold to dynamic range of signal
-    short mx = getMax(baselineBuffer);
-    short mn = getMin(baselineBuffer);
+    unsigned short mx = getMax(baselineBuffer);
+    unsigned short mn = getMin(baselineBuffer);
     jump_threshold = min(max(2 * (mx - mn), MIN_THRESHOLD), MAX_THRESHOLD);
     
     baseline[currentSensor] = computeAverage(baselineBuffer[currentSensor], BUFFER_SIZE);
@@ -266,18 +266,18 @@ void loop() {
   }
 }
 
-short computeAverage(short a[], int aSize) {
-  long sum = 0;
+unsigned short computeAverage(unsigned short a[], long aSize) {
+  unsigned long sum = 0;
   for (int i = 0; i < aSize; i++) {
     sum = sum + a[i];
   }
-  short toreturn = (short) (sum / aSize);
+  unsigned short toreturn = (unsigned short) (sum / aSize);
 
   return toreturn;
 }
 
-short getMax(short numarray[NUM_SENSORS][BUFFER_SIZE]) {
-  short mx = numarray[0][0];
+unsigned short getMax(unsigned short numarray[NUM_SENSORS][BUFFER_SIZE]) {
+  unsigned short mx = numarray[0][0];
   for (int i = 0; i < NUM_SENSORS; i++) {
     for (int j = 0; j < BUFFER_SIZE; j++) {
       if (mx < numarray[i][j]) {
@@ -289,8 +289,8 @@ short getMax(short numarray[NUM_SENSORS][BUFFER_SIZE]) {
 }
 
 
-short getMin(short numarray[NUM_SENSORS][BUFFER_SIZE]) {
-  short mn = numarray[0][0];
+unsigned short getMin(unsigned short numarray[NUM_SENSORS][BUFFER_SIZE]) {
+  unsigned short mn = numarray[0][0];
   for (int i = 0; i < NUM_SENSORS; i++) {
     for (int j = 0; j < BUFFER_SIZE; j++) {
       if (mn > numarray[i][j]) {
