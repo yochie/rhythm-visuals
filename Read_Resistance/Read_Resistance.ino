7482a5c8a2531329bf30067f6d5e5524f9d0b3f4
yoann.ochietti@gmail.com <yoann.ochietti@gmail.com>
2017-12-08 17:21:48 -0500

    Added loop too check every sensor at each iteration of the maion loop().

--- Read_Resistance/Read_Resistance.ino
+++ Read_Resistance/Read_Resistance.ino
@@ -88,9 +88,6 @@ unsigned short lastVal[NUM_SENSORS];
 //current threshold
 unsigned short jump_threshold[NUM_SENSORS];
 
-//current pin index
-unsigned short currentSensor;
-
 //timing variable for analysis of execution times
 //used for debug
 //unsigned long lastTime;
@@ -110,169 +107,164 @@ void setup() {
   memset(toWait, 0, sizeof(toWait));
   memset(baseline, 1024, sizeof(baseline));
   memset(lastVal, 2048, sizeof(lastVal));
-  memset(jump_threshold, (MIN_THRESHOLD+MAX_THRESHOLD/2), sizeof(jump_threshold));
+  memset(jump_threshold, (MIN_THRESHOLD + MAX_THRESHOLD / 2), sizeof(jump_threshold));
 }
 
 void loop() {
-  //If baseline buffer is full, compute its average and reset its counter
-  if (baselineCount[currentSensor] > (BUFFER_SIZE - 1) * CYCLES_PER_BASELINE) {
-
-    //adjust threshold to dynamic range of signal
-    unsigned short mx = getMax(baselineBuffer);
-    unsigned short mn = getMin(baselineBuffer);
-    jump_threshold[currentSensor] = min(max(2 * (mx - mn), MIN_THRESHOLD), MAX_THRESHOLD);
-
-    //    lastTime = micros();
-    baseline[currentSensor] = computeAverage(baselineBuffer[currentSensor], BUFFER_SIZE);
-    //    unsigned long delta = micros() - lastTime;
-    //    Serial.println(delta);
-
-    //    Serial.print("Computed new baseline : !!!!!!!!!!!!!!!!!!!!!!!!1");
-    //    Serial.println(baseline[currentSensor]);
-    //    delay(3000);
-
-    baselineCount[currentSensor] = 0;
-  }
-
-  //New Val
-  unsigned short val = (unsigned short) analogRead(PINS[currentSensor]);
-  unsigned short jumpVal = max(0,val - baseline[currentSensor]);
-
-  //Printing graph for arduino plotter
-  Serial.print((String) val);
-  Serial.print(" ");
-  Serial.print((String) baseline[currentSensor]);
-  Serial.print(" ");
-  Serial.print((String) (baseline[currentSensor] + jump_threshold[currentSensor]));
-  Serial.print(" ");
-  Serial.print(0);
-  Serial.print(" ");
-  Serial.println(500);
-  delay(1);
-  //  Serial.println((String) baseline[currentSensor]);
-
-  //JUMPING
-  //If jump is large enough, save val to buffer and print average jump if its full.
-  //Also makes sure that we don't get stuck in jump by restablishing baseline after some stagnation (MAX_CONSECUTIVE_JUMPS)
-  if (jumpVal >= jump_threshold[currentSensor]) {
-
-    //CONSECUTIVE
-    //2048 is default value for lastVal, so it will never match on this condition
-    //since val is between [0, 1024] and variability should be no larger than 1024
-    //ignores first jump signal, but shouldn't really matter as we're calculating average...
-    if (abs(lastVal[currentSensor] - val) < min(JUMP_VARIABILITY, 1024)) {
-      //RESET BASELINE
-      //If we get many consecutive jumps without enough variability, reset baseline.
-      if (cJumpIndex[currentSensor] >= CJUMP_BUFFER_SIZE) {
-
-        unsigned short avg = computeAverage(cJumpBuffer[currentSensor], CJUMP_BUFFER_SIZE);
-
-        //raise average a little before resetting baseline to it: early jump vals tend to make
-        //the average too low for the pressure by the time it resets, causing constant jumps
-        //TODO: add delay to consecutive jump filling so that it ignores first part of any jump
-        //That might be enough to remove this "hack"
-        baseline[currentSensor] = min(1024, avg * 1.25);
-
-        //Serial.println("Consecutive RESET");
-        //Serial.println(baseline[currentSensor]);
-
+  for (unsigned short currentSensor = 0; currentSensor < NUM_SENSORS; currentSensor++){
+    //If baseline buffer is full, compute its average and reset its counter
+    if (baselineCount[currentSensor] > (BUFFER_SIZE - 1) * CYCLES_PER_BASELINE) {
+  
+      //adjust threshold to dynamic range of signal
+      unsigned short mx = getMax(baselineBuffer);
+      unsigned short mn = getMin(baselineBuffer);
+      jump_threshold[currentSensor] = min(max(2 * (mx - mn), MIN_THRESHOLD), MAX_THRESHOLD);
+  
+      //    lastTime = micros();
+      baseline[currentSensor] = computeAverage(baselineBuffer[currentSensor], BUFFER_SIZE);
+      //    unsigned long delta = micros() - lastTime;
+      //    Serial.println(delta);
+  
+      //    Serial.print("Computed new baseline : !!!!!!!!!!!!!!!!!!!!!!!!1");
+      //    Serial.println(baseline[currentSensor]);
+      //    delay(3000);
+  
+      baselineCount[currentSensor] = 0;
+    }
+  
+    //New Val
+    unsigned short val = (unsigned short) analogRead(PINS[currentSensor]);
+    unsigned short jumpVal = max(0, val - baseline[currentSensor]);
+  
+    //Printing graph for arduino plotter
+    Serial.print((String) val);
+    Serial.print(" ");
+    Serial.print((String) baseline[currentSensor]);
+    Serial.print(" ");
+    Serial.print((String) (baseline[currentSensor] + jump_threshold[currentSensor]));
+    Serial.print(" ");
+    Serial.print(0);
+    Serial.print(" ");
+    Serial.println(500);
+    delay(1);
+    //  Serial.println((String) baseline[currentSensor]);
+  
+    //JUMPING
+    //If jump is large enough, save val to buffer and print average jump if its full.
+    //Also makes sure that we don't get stuck in jump by restablishing baseline after some stagnation (MAX_CONSECUTIVE_JUMPS)
+    if (jumpVal >= jump_threshold[currentSensor]) {
+  
+      //CONSECUTIVE
+      //2048 is default value for lastVal, so it will never match on this condition
+      //since val is between [0, 1024] and variability should be no larger than 1024
+      //ignores first jump signal, but shouldn't really matter as we're calculating average...
+      if (abs(lastVal[currentSensor] - val) < min(JUMP_VARIABILITY, 1024)) {
+        //RESET BASELINE
+        //If we get many consecutive jumps without enough variability, reset baseline.
+        if (cJumpIndex[currentSensor] >= CJUMP_BUFFER_SIZE) {
+  
+          unsigned short avg = computeAverage(cJumpBuffer[currentSensor], CJUMP_BUFFER_SIZE);
+  
+          //raise average a little before resetting baseline to it: early jump vals tend to make
+          //the average too low for the pressure by the time it resets, causing constant jumps
+          //TODO: add delay to consecutive jump filling so that it ignores first part of any jump
+          //That might be enough to remove this "hack"
+          baseline[currentSensor] = min(1024, avg * 1.25);
+  
+          //Serial.println("Consecutive RESET");
+          //Serial.println(baseline[currentSensor]);
+  
+          cJumpCount[currentSensor] = 0;
+          cJumpIndex[currentSensor] = 0;
+          jumpIndex[currentSensor] = 0;
+          lastVal[currentSensor] = 2048;
+          jumped[currentSensor] = false;
+          baselineCount[currentSensor] = 0;
+          return;
+        }
+  
+        if (cJumpCount[currentSensor] % CYCLES_PER_CJUMP == 0) {
+          cJumpBuffer[currentSensor][cJumpIndex[currentSensor]] = val;
+          cJumpIndex[currentSensor]++;
+        }
+        cJumpCount[currentSensor]++;
+      }
+      //VARYING
+      else {
         cJumpCount[currentSensor] = 0;
         cJumpIndex[currentSensor] = 0;
-        jumpIndex[currentSensor] = 0;
-        lastVal[currentSensor] = 2048;
-        jumped[currentSensor] = false;
-        baselineCount[currentSensor] = 0;
-        return;
       }
-
-      if (cJumpCount[currentSensor] % CYCLES_PER_CJUMP == 0) {
-        cJumpBuffer[currentSensor][cJumpIndex[currentSensor]] = val;
-        cJumpIndex[currentSensor]++;
+  
+      //PLACE JUMP IN BUFFER
+      //If there is place in buffer, add jump there.
+      if (jumpIndex[currentSensor] < JUMP_BUFFER_SIZE) {
+  
+        //put absolute val (not jumpVal) in buffer
+        jumpBuffer[currentSensor][jumpIndex[currentSensor]] = val;
+        jumpIndex[currentSensor]++;
+  
+        //mark as jump requiring blowback compensation
+        if (!jumped[currentSensor] && cJumpIndex[currentSensor] > MIN_JUMPS ) {
+          jumped[currentSensor] = true;
+        }
+  
+        //store current val for stagnation check
+        lastVal[currentSensor] = val;
+  
+        //BUFFER FULL
+        //If buffer is full, print buffer average to serial and reset counter
+        //This means we send one value every JUMP_BUFFER_SIZE iterations of the
+        //main loop if the button is held down. Careful: making JUMP_BUFFER_SIZE too large
+        //would cause short presses to be ignored.
+        if ( jumpIndex[currentSensor] == JUMP_BUFFER_SIZE) {
+          short avgVal = computeAverage(jumpBuffer[currentSensor], jumpIndex[currentSensor]);
+          //Serial.print("average from buffer ");
+          //Serial.print("J");
+          //Serial.print(currentSensor);
+          //Serial.print(": ");
+          //Serial.println(constrain(avgVal - baseline[currentSensor], 0, 1024));
+  
+          jumpIndex[currentSensor] = 0;
+          lastVal[currentSensor] = val;
+        }
       }
-      cJumpCount[currentSensor]++;
+  
     }
-    //VARYING
+    //NOT JUMPING
+    //Add value to buffer for baseline update and reset jump counting variables
     else {
+      //if last cycle was jump, send 0 val to signify jump is over
+      if (jumped[currentSensor]) {
+        jumped[currentSensor] = false;
+        //Stop computing baseline for a while
+        //because sensor values tend to be lower than baseline after releasing the button
+        toWait[currentSensor] = JUMP_BLOWBACK;
+      }
+  
+      if (toWait[currentSensor] == 0) {
+        //every x loops, add value to baseline buffer for updating baseline
+        if (baselineCount[currentSensor] % CYCLES_PER_BASELINE == 0) {
+          baselineBuffer[currentSensor][(unsigned short) (baselineCount[currentSensor] / CYCLES_PER_BASELINE)] = val;
+        }
+        baselineCount[currentSensor]++;
+  
+      } else {
+        toWait[currentSensor]--;
+      }
+      // If using graphing, you might want to use these lines
+      //Serial.print("J");
+      //Serial.print(currentSensor);
+      //Serial.println(": 0");
+      //delay(1);
+  
+      //Using 2048 as default value that will never match the current val when testing for consecutive jumps
+      lastVal[currentSensor] = 2048;
+  
+      //Reset jump counters
+      jumpIndex[currentSensor] = 0;
       cJumpCount[currentSensor] = 0;
       cJumpIndex[currentSensor] = 0;
     }
-
-    //PLACE JUMP IN BUFFER
-    //If there is place in buffer, add jump there.
-    if (jumpIndex[currentSensor] < JUMP_BUFFER_SIZE) {
-
-      //put absolute val (not jumpVal) in buffer
-      jumpBuffer[currentSensor][jumpIndex[currentSensor]] = val;
-      jumpIndex[currentSensor]++;
-
-      //mark as jump requiring blowback compensation
-      if (!jumped[currentSensor] && cJumpIndex[currentSensor] > MIN_JUMPS ) {
-        jumped[currentSensor] = true;
-      }
-
-      //store current val for stagnation check
-      lastVal[currentSensor] = val;
-
-      //BUFFER FULL
-      //If buffer is full, print buffer average to serial and reset counter
-      //This means we send one value every JUMP_BUFFER_SIZE iterations of the
-      //main loop if the button is held down. Careful: making JUMP_BUFFER_SIZE too large
-      //would cause short presses to be ignored.
-      if ( jumpIndex[currentSensor] == JUMP_BUFFER_SIZE) {
-        short avgVal = computeAverage(jumpBuffer[currentSensor], jumpIndex[currentSensor]);
-        //Serial.print("average from buffer ");
-        //Serial.print("J");
-        //Serial.print(currentSensor);
-        //Serial.print(": ");
-        //Serial.println(constrain(avgVal - baseline[currentSensor], 0, 1024));
-
-        jumpIndex[currentSensor] = 0;
-        lastVal[currentSensor] = val;
-      }
-    }
-
-  }
-  //NOT JUMPING
-  //Add value to buffer for baseline update and reset jump counting variables
-  else {
-    //if last cycle was jump, send 0 val to signify jump is over
-    if (jumped[currentSensor]) {
-      jumped[currentSensor] = false;
-      //Stop computing baseline for a while
-      //because sensor values tend to be lower than baseline after releasing the button
-      toWait[currentSensor] = JUMP_BLOWBACK;
-    }
-
-    if (toWait[currentSensor] == 0) {
-      //every x loops, add value to baseline buffer for updating baseline
-      if (baselineCount[currentSensor] % CYCLES_PER_BASELINE == 0) {
-        baselineBuffer[currentSensor][(unsigned short) (baselineCount[currentSensor] / CYCLES_PER_BASELINE)] = val;
-      }
-      baselineCount[currentSensor]++;
-
-    } else {
-      toWait[currentSensor]--;
-    }
-    // If using graphing, you might want to use these lines
-    //Serial.print("J");
-    //Serial.print(currentSensor);
-    //Serial.println(": 0");
-    //delay(1);
-
-    //Using 2048 as default value that will never match the current val when testing for consecutive jumps
-    lastVal[currentSensor] = 2048;
-
-    //Reset jump counters
-    jumpIndex[currentSensor] = 0;
-    cJumpCount[currentSensor] = 0;
-    cJumpIndex[currentSensor] = 0;
-  }
-  //switch to next sensor
-  if (currentSensor < NUM_SENSORS - 1) {
-    currentSensor++;
-  }
-  else {
-    currentSensor = 0;
   }
 }
 
