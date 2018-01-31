9898d3c6ee79ece0f2e69b7591858120f314c8bb
yoann <yoann.ochietti@gmail.com>
2018-01-31 16:53:07 -0500

    Added seperate timings for each signal type rather than sharing timers. Only the lastRisingTime is used by different signals.

--- Read_Resistance/Read_Resistance.ino
+++ Read_Resistance/Read_Resistance.ino
@@ -1,68 +1,5 @@
 #include <limits.h>
 
-//*CONFIGURATION CONSTANTS*
-
-//Sensor pins
-//At least one plz...
-const int NUM_SENSORS = 4;
-const int SENSOR_PINS[NUM_SENSORS] = {0, 1, 2, 3};
-
-const boolean DEBUG = false;
-
-/*MIDI CONFIG*/
-const boolean WITH_MIDI = true;
-const int NOTES[NUM_SENSORS] = {60, 62, 64, 65};
-const int MIDI_CHANNEL = 1;
-
-/*MOTOR CONFIG*/
-const boolean WITH_MOTORS = true;
-const int NUM_MOTORS = 1;
-const int MOTOR_PINS[NUM_MOTORS] = {13};
-unsigned const long MAX_MOTOR_PULSE_DURATION = 100000;
-
-//MAX_THRESHOLD is used when the baseline is very unstable
-const int MAX_THRESHOLD = 150;
-
-//MIN_THRESHOLD is used when the baseline is very stable
-const int MIN_THRESHOLD = 150;
-
-//Time between threshold traversal and rising() signal
-//Allows for velocity measurment and ignoring very short jumps
-unsigned const long NOTE_VELOCITY_DELAY = 2000;
-
-//Delay in microseconds after sending falling signal
-//for which no more signals are sent for that sensor
-unsigned const long NOTE_OFF_DELAY = 10000;
-
-//Delay in microseconds between sustain signals
-unsigned const long SUSTAIN_DELAY = 50000;
-
-//Delay in micro seconds between baseline samples
-unsigned const long BASELINE_SAMPLE_DELAY = 500;
-
-//Delay in microseconds adter each line of debug messages
-//Blocking (uses delay() function)
-//Prevents overloading serial communications
-const int PRINT_DELAY = 50;
-
-//number of microseconds after jump during which baseline update is paused
-unsigned const long BASELINE_BLOWBACK_DELAY = 2000;
-
-//amount of baseline samples that we average baseline over
-//Multiply with BASELINE_SAMPLE_DELAY to get baseline update duration.
-const int BASELINE_BUFFER_SIZE = 1000;
-
-//number of samples removed from baseline buffer when jump is over
-//This is used to prevent by removing rising edge portion of signal
-//that is below threshold from weighing in on baseline.
-//Making too large would prevent baseline update while fast-tapping.
-//Multiply with BASELINE_SAMPLE_DELAY  to get the rise time to reach the threshold.
-const int RETRO_JUMP_BLOWBACK_SAMPLES = 3000 / BASELINE_SAMPLE_DELAY;
-
-//After this amount of sustains
-//the baseline is reset to that jump sequences avg velocity
-const int MAX_CONSECUTIVE_SUSTAINS = 10 * 1000000 / SUSTAIN_DELAY;
-
 //*SYSTEM CONSTANTS*
 //these shouldn't have to be modified
 
@@ -73,10 +10,83 @@ const int BAUD_RATE = 115200;
 //Always 1023 for arduino
 const int MAX_READING = 1023;
 
-//*GLOBAL VARIABLES*
-//would love to make them static and local to loop(), but they need to be initialized to non-zero values
-//std::vector allows non-zero initialization, but I'm not sure I should include it just for this purpose...
+//Used to improve readability of configuration
+//Gives number in microseconds since that is the timing unit
+const int MICROSECOND = 1;
+const int MILLISECOND = 1000;
+const int SECOND = 1000000;
 
+//*CONFIGURATION CONSTANTS*
+
+//Sensor pins
+//At least one plz...
+const int NUM_SENSORS = 4;
+const int SENSOR_PINS[NUM_SENSORS] = {0, 1, 2, 3};
+
+const boolean DEBUG = true;
+
+/*MIDI CONFIG*/
+const boolean WITH_MIDI = true;
+const int NOTES[NUM_SENSORS] = {60, 62, 64, 65};
+const int MIDI_CHANNEL = 1;
+
+/*MOTOR CONFIG*/
+const boolean WITH_MOTORS = true;
+const int NUM_MOTORS = 1;
+const int MOTOR_PINS[NUM_MOTORS] = {13};
+unsigned const long MAX_MOTOR_PULSE_DURATION = 100 * MILLISECOND;
+
+//MAX_THRESHOLD is used when the baseline is very unstable
+const int MAX_THRESHOLD = 150;
+
+//MIN_THRESHOLD is used when the baseline is very stable
+const int MIN_THRESHOLD = 150;
+
+//Time between threshold traversal and rising() signal
+//Allows for velocity measurment and ignoring very short jumps
+unsigned const long NOTE_VELOCITY_DELAY = 0.5 * MILLISECOND;
+
+//Delay in microseconds after sending rising() signal
+//for which no more signals are sent for that sensor
+unsigned const long NOTE_ON_DELAY = 25 * MILLISECOND;
+
+//Delay in microseconds after sending falling() signal
+//for which no more signals are sent for that sensor
+unsigned const long NOTE_OFF_DELAY = 25 * MILLISECOND;
+
+//Delay in microseconds between sustain() signals
+unsigned const long SUSTAIN_DELAY = 50 * MILLISECOND;
+
+//Delay in micro seconds between baseline samples
+unsigned const long BASELINE_SAMPLE_DELAY = 0.5 * MILLISECOND;
+
+//Delay in microseconds adter each line of debug messages
+//Blocking (uses delay() function)
+//Prevents overloading serial communications
+const int PRINT_DELAY = 50 * MICROSECOND;
+
+//number of microseconds after jump during which baseline update is paused
+unsigned const long BASELINE_BLOWBACK_DELAY = 2 * MILLISECOND;
+
+//TODO: change constant to timing notation
+//amount of baseline samples that we average baseline over
+//Multiply with BASELINE_SAMPLE_DELAY to get baseline update duration.
+const int BASELINE_BUFFER_SIZE = 1000;
+
+//TODO: move division to the code
+//number of samples removed from baseline buffer when jump is over
+//This is used to prevent rising edge portion of signal
+//that is below threshold from weighing in on baseline.
+//Making too large would prevent baseline update while fast-tapping.
+//Multiply with BASELINE_SAMPLE_DELAY  to get the rise time to reach the threshold.
+const int RETRO_JUMP_BLOWBACK_SAMPLES = (1 * MILLISECOND) / BASELINE_SAMPLE_DELAY;
+
+//TODO: move division to the code
+//After this amount of sustains
+//the baseline is reset to the last sensor reading
+const int MAX_CONSECUTIVE_SUSTAINS = (10 * SECOND) / SUSTAIN_DELAY;
+
+//*GLOBAL VARIABLES*
 //current baseline for each pin
 int baseline[NUM_SENSORS];
 
@@ -116,11 +126,14 @@ void loop() {
   static unsigned long toWaitBeforeBaseline[NUM_SENSORS];
 
   //used to delay midi signals from one another
-  static unsigned long toWaitBeforeSignal[NUM_SENSORS];
+  static unsigned long toWaitBeforeRising[NUM_SENSORS];
+  static unsigned long toWaitBeforeFalling[NUM_SENSORS];
+  static unsigned long toWaitBeforeSustaining[NUM_SENSORS];
 
   //used to calculate time difference in microseconds while waiting
-  static unsigned long lastSignalTime[NUM_SENSORS];
+  static unsigned long lastRisingTime[NUM_SENSORS];
   static unsigned long lastBaselineTime[NUM_SENSORS];
+  static unsigned long lastSustainingTime[NUM_SENSORS];
 
   //*STACK VARIABLES*
   int toPrint[NUM_SENSORS];
@@ -137,69 +150,80 @@ void loop() {
 
     //JUMPING
     if (distanceAboveBaseline >= jumpThreshold[currentSensor]) {
-      //WAITING
-      if (toWaitBeforeSignal[currentSensor] > 0) {
-        updateRemainingTime(toWaitBeforeSignal[currentSensor], lastSignalTime[currentSensor]);
+      //VELOCITY OFFSET
+      if (sustainCount[currentSensor] == 0) {
+        lastRisingTime[currentSensor] = micros();
+        toWaitBeforeRising[currentSensor] = NOTE_VELOCITY_DELAY;
+        sustainCount[currentSensor]++;
       }
-      //STAGNATION RESET
-      else if (sustainCount[currentSensor] > MAX_CONSECUTIVE_SUSTAINS) {
-        baseline[currentSensor] = sensorReading;
-
-        //reset counters
-        baselineBufferIndex[currentSensor] = 0;
-        sustainCount[currentSensor] = 0;
-      }
-      //SIGNALING
-      else {
-        //VELOCITY_DELAY
-        if (sustainCount[currentSensor] == 0) {
-          lastSignalTime[currentSensor] = micros();
-          toWaitBeforeSignal[currentSensor] = NOTE_VELOCITY_DELAY;
+      //RISING
+      else if (sustainCount[currentSensor] == 1) {
+        if (toWaitBeforeRising[currentSensor] > 0) {
+          updateRemainingTime(toWaitBeforeRising[currentSensor], lastRisingTime[currentSensor]);
         }
-        //RISING
-        else if (sustainCount[currentSensor] == 1) {
+        else {
           rising(currentSensor, distanceAboveBaseline);
 
-          lastSignalTime[currentSensor] = micros();
-          toWaitBeforeSignal[currentSensor] = SUSTAIN_DELAY;
+          lastRisingTime[currentSensor] = micros();
+          toWaitBeforeFalling[currentSensor] = NOTE_ON_DELAY;
+
+          lastSustainingTime[currentSensor] = micros();
+          toWaitBeforeSustaining[currentSensor] = SUSTAIN_DELAY;
+
           justJumped[currentSensor] = true;
           sustainCount[currentSensor]++;
         }
-        //SUSTAIN
+      }
+      //SUSTAINING
+      else {
+        if (sustainCount[currentSensor] > MAX_CONSECUTIVE_SUSTAINS) {
+          baseline[currentSensor] = sensorReading;
+
+          //reset counters
+          baselineBufferIndex[currentSensor] = 0;
+          sustainCount[currentSensor] = 0;
+        }
+        else if (toWaitBeforeSustaining[currentSensor] > 0) {
+          updateRemainingTime(toWaitBeforeSustaining[currentSensor], lastSustainingTime[currentSensor]);
+        }
         else {
           sustained(currentSensor, distanceAboveBaseline, NOTE_VELOCITY_DELAY + ((sustainCount[currentSensor] - 1) * SUSTAIN_DELAY));
 
-          lastSignalTime[currentSensor] = micros();
-          toWaitBeforeSignal[currentSensor] = SUSTAIN_DELAY;
+          lastSustainingTime[currentSensor] = micros();
+          toWaitBeforeSustaining[currentSensor] = SUSTAIN_DELAY;
           sustainCount[currentSensor]++;
         }
-
-        //jump counter
-        sustainCount[currentSensor]++;
       }
     }
     //BASELINING
     else {
       //FALLING
       if (justJumped[currentSensor]) {
-        falling(currentSensor);
+        //WAIT_BEFORE_FALLING
+        if (toWaitBeforeFalling[currentSensor]) {
+          updateRemainingTime(toWaitBeforeFalling[currentSensor], lastRisingTime[currentSensor]);
+        }
+        //FALL
+        else {
+          falling(currentSensor);
 
-        //wait before sending more midi signals
-        //debounces falling edge
-        lastSignalTime[currentSensor] = micros();
-        toWaitBeforeSignal[currentSensor] = NOTE_OFF_DELAY;
+          //wait before sending more midi signals
+          //debounces falling edge
+          lastRisingTime[currentSensor] = micros();
+          toWaitBeforeRising[currentSensor] = NOTE_OFF_DELAY;
 
-        //wait before buffering baseline
-        //this is to ignore the sensor "blowback" (erratic readings after jumps)
-        //and remove falling edge portion of signal that is below threshold
-        lastBaselineTime[currentSensor] = micros();
-        toWaitBeforeBaseline[currentSensor] = BASELINE_BLOWBACK_DELAY;
+          //wait before buffering baseline
+          //this is to ignore the sensor "blowback" (erratic readings after jumps)
+          //and remove falling edge portion of signal that is below threshold
+          lastBaselineTime[currentSensor] = micros();
+          toWaitBeforeBaseline[currentSensor] = BASELINE_BLOWBACK_DELAY;
 
-        justJumped[currentSensor] = false;
+          justJumped[currentSensor] = false;
 
-        //backtrack baseline count to remove jump start
-        //(might not do anything if we just updated baseline)
-        baselineBufferIndex[currentSensor] = max( 0, baselineBufferIndex[currentSensor] - RETRO_JUMP_BLOWBACK_SAMPLES);
+          //backtrack baseline count to remove jump start
+          //(might not do anything if we just updated baseline)
+          baselineBufferIndex[currentSensor] = max( 0, baselineBufferIndex[currentSensor] - RETRO_JUMP_BLOWBACK_SAMPLES);
+        }
       }
       //WAIT BEFORE BASELINING
       else if (toWaitBeforeBaseline[currentSensor] > 0) {
@@ -226,12 +250,11 @@ void loop() {
       //reset jump counter
       sustainCount[currentSensor] = 0;
     }
-  }
-  if (DEBUG) {
-    printResults(toPrint, sizeof(toPrint) / sizeof(int));
+    if (DEBUG) {
+      printResults(toPrint, sizeof(toPrint) / sizeof(int));
+    }
   }
 }
-
 //*HELPERS*
 
 int bufferAverage(int * a, int aSize) {
