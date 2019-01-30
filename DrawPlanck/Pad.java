af6765923fc3f4fe1478b7d6f62002968b60049b
Yoann Ochietti <yoann.ochietti@gmail.com>
2019-01-29 22:14:11 -0500

    Pad class added to store static pad info and offer utility methods.

---
+++ DrawPlanck/Pad.java
@@ -0,0 +1,28 @@
+import java.util.Map;
+import java.util.HashMap; 
+
+public class Pad {
+  static int lastIndex = 0; 
+  public String name;
+  public int index;
+  public int note;
+  public boolean isAux;
+  
+  private static Map<Integer, Integer> noteToPadMap = new HashMap();
+  
+  public static int noteToPad(int note){
+    int toReturn = -1;
+    if (noteToPadMap.containsKey(note)) {
+      toReturn = noteToPadMap.get(note);
+    }
+    return toReturn;
+  }
+
+  public Pad(String name, int note, boolean isAux) {
+    this.name = name;
+    this.note = note;
+    this.index = lastIndex++;
+    this.isAux = isAux;
+    noteToPadMap.put(note, this.index);
+  }
+}
